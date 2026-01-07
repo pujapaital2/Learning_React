d375d47a51648cc4b33295e4f93d9098622722ad
punyatwaya <nayakpunatoya@gmail.com>
2026-01-07 22:11:07 +0530

    Day 1:react into,JSX,components,props

---
+++ Punyatwaya_53/day01-react-basics/src/App.jsx
@@ -0,0 +1,12 @@
+import Header from './components/Header'
+
+function App() {
+  return (
+    <div>
+      <Header title="React Intro & Tooling" />
+      <p>JSX, components, props implemented</p>
+    </div>
+  )
+}
+
+
