128c43d2308206438db55b3b3c743afa4d4a0357
Pujasmruti Satapathy <satapathypujasmruti@gmail.com>
2026-01-10 14:59:57 +0530

    Day 5: Forms and Validation in React

---
+++ Pujasmruti_52/day05-react-Forms/src/App.jsx
@@ -0,0 +1,12 @@
+import FormValidation from "./day-5/FormValidation";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <FormValidation />
+    </div>
+  );
+}
+
+export default App;
