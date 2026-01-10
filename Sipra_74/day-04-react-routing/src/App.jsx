1de912fd88e0e55e1ba10dba86f6bd14e7ffa3d4
sipra Routray <sipraroutray88@gmail.com>
2026-01-10 12:17:31 +0530

    Day-4: React Router v6 multi-page routing

---
+++ Sipra_74/day-04-react-routing/src/App.jsx
@@ -0,0 +1,26 @@
+import { Routes, Route, Link } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
+
+function App() {
+  return (
+    <div>
+      <h1>React Router v6</h1>
+
+      <nav>
+        <Link to="/">Home</Link> |{" "}
+        <Link to="/about">About</Link> |{" "}
+        <Link to="/contact">Contact</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
